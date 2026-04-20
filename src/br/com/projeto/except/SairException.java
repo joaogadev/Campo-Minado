497a7bd4d922ba2949212ba80b611bdcfaaf0165
João Gabriel <joaogabriel.trindade04@gmail.com>
2026-04-20 11:51:09 -0300

    exceção para caso o usuario saia do jogo

---
+++ src/br/com/projeto/except/SairException.java
@@ -0,0 +1,5 @@
+package br.com.projeto.except;
+
+public class SairException extends RuntimeException {
+    private static final long serialVersionUID = 1;
+}
---
+++ src/br/com/projeto/visual/TabuleiroConsole.java
@@ -0,0 +1,4 @@
+package br.com.projeto.visual;
+
+public class TabuleiroConsole {
+}
