169083c97b939e0b912d24f49b86a8949c781dda
João Gabriel <joaogabriel.trindade04@gmail.com>
2026-04-24 17:16:15 -0300

    Execução do tabuleiro

--- src/br/com/projeto/Main.java
+++ src/br/com/projeto/Main.java
@@ -1,4 +1,13 @@
 package br.com.projeto;
 
+import br.com.projeto.model.Tabuleiro;
+import br.com.projeto.visual.TabuleiroConsole;
+
 public class Main {
+    public static void main(String[] args) {
+        Tabuleiro tab = new Tabuleiro(6, 6, 6);
+
+        new TabuleiroConsole(tab);
+
+    }
 }
